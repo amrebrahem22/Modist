06b8947b57c76e7480ee73750d1fea1373efa354
amrebrahem22 <amrebrahem226@gmail.com>
2020-01-24 06:18:21 +0200

    domain2

--- modist/settings/base.py
+++ modist/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(6idhge8@rs5(5)xat)sadgna$79)pku71y_rjw73!2tdz)h6p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['modist.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
