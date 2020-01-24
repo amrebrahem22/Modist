8fd57c8aba21b94660bba3bc20bb9dddd026f8d9
amrebrahem22 <amrebrahem226@gmail.com>
2020-01-24 08:01:45 +0200

    domain2

--- accounts/models.py
+++ accounts/models.py
@@ -67,6 +67,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
+    username = None
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
     email = models.EmailField(
