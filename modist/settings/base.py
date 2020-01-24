65c709df8f46a738ee3ff575433e663f222fb6c1
amrebrahem22 <amrebrahem226@gmail.com>
2020-01-24 04:10:36 +0200

    Start Dyployment

--- modist/settings/base.py
+++ modist/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -198,3 +198,14 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 ADMIN = [
     ('Amr Ibrahim', EMAIL_HOST_USER),
 ]
+
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
